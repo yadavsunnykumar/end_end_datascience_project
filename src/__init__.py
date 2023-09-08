16ef94d2ec7e0f47ab393597b3a9369f1c47a1c3
sunny kumar yadav <100453782+yadav1233@users.noreply.github.com>
2023-09-08 13:05:57 +0545

    creating file pipeline

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-print("This is The Data Science end to end project")
-x=10
-y=20
-print(f'The sum of {x} and {y} is {x+y}')
---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+project_name = "mlproject"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformtion.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitoring.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "app.py",
+    "Dockerfile"
+    ]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory {filedir} for the file {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"creating empty file:{filepath}")
+    else:
+        logging.info(f"{filename} already exists")
